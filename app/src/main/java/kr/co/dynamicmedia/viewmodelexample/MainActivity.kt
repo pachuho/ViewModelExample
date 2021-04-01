7db7eb24ef7470dbdd3ef1b4085edd4c6dc51918
park juho <blueblack96@dynamicmedia.co.kr>
2021-04-01 11:49:11 +0900

    aac vm

--- app/src/main/java/kr/co/dynamicmedia/viewmodelexample/MainActivity.kt
+++ app/src/main/java/kr/co/dynamicmedia/viewmodelexample/MainActivity.kt
@@ -6,12 +6,26 @@
     import android.widget.Button
     import android.widget.TextView
     import androidx.databinding.DataBindingUtil
+    import androidx.lifecycle.Observer
+    import androidx.lifecycle.ViewModelProvider
     import kr.co.dynamicmedia.viewmodelexample.databinding.ActivityMainBinding
 
     class MainActivity : AppCompatActivity() {
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
+            // 데이터 바인딩(ViewModel 연결)
             val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
             binding.vm = ViewModel()
+
+            // AAC ViewModel
+            val viewModel = ViewModelProvider(this).get(AccViewModel::class.java)
+            viewModel.getInputText().observe(this, Observer <String>{ newStr ->
+                binding.tvSample.text = newStr
+
+            })
+
+            binding.btn1.setOnClickListener {
+                viewModel.updateText(binding.et1.text.toString())
+            }
         }
     }
